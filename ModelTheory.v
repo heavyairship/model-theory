8b4431a26dc44acc5b9923bb17800ec9140ae700
afichman <andrew.fichman@gmail.com>
2019-04-02 09:58:57 -0400

    Fixed var issue

--- ModelTheory.v
+++ ModelTheory.v
@@ -1,24 +1,31 @@
 Require Export Coq.Strings.String.
 
-Inductive term : Type := (* FixMe: does this need a type param? *)
-| Const : string -> term
-| Func  : string -> (list term) -> term. (*FixMe: how to define arg list as non-empty?*)
+Inductive var : Type :=
+| Var : string -> var.
 
-Inductive formula {A : Type} : Type :=
+(* FixMe: does this need a type param? *)
+Inductive term : Type :=
+| VarT   : var -> term
+| ConstT : string -> term
+| FuncT  : string -> (list term) -> term. (*FixMe: how to define arg list as non-empty? 
+                                            FuncT w/ empty arg list is same as a constant. *)
+
+(* FixMe: does this need a type param? *)
+Inductive formula : Type := 
 | Equals  : term -> term -> formula
 | Relates : string -> (list term) -> formula
 | Not     : formula -> formula
 | Or      : formula -> formula -> formula
-| Forall  : (A -> formula) -> formula.
+| Forall  : var -> formula -> formula.
 
-Definition And {A: Type} (phi psi : formula) := @Not A (Or (Not phi) (Not psi)).
-Definition Impl {A: Type} (phi psi : formula) := @Or A (Not phi) psi.
-Definition Iff {A : Type} (phi psi : formula) := @And A (Impl phi psi) (Impl psi phi).
-Definition Exists {A : Type} (f : A -> formula) := @Not A (Forall (fun a => Not (f a))).
+Definition And (phi psi : formula) := Not (Or (Not phi) (Not psi)).
+Definition Impl (phi psi : formula) := Or (Not phi) psi.
+Definition Iff (phi psi : formula) := And (Impl phi psi) (Impl psi phi).
+Definition Exists (v : var) (phi : formula) := Not (Forall v (Not phi)).
 
-Inductive subformula {A: Type} : @formula A -> @formula A -> Prop :=
+Inductive subformula : formula -> formula -> Prop :=
 | SubIden   (psi phi : formula) : psi = phi -> subformula psi phi
 | SubNot    (psi phi : formula) : subformula (Not psi) phi -> subformula psi phi
 | SubOrL    (psi theta phi : formula) : subformula (Or psi theta) phi -> subformula psi phi
 | SubOrR    (psi theta phi : formula) : subformula (Or psi theta) phi -> subformula theta phi
-| SubForall (psi : A -> formula) (phi: formula) : subformula (Forall psi) phi -> ???.
+| SubForall (v : var) (psi phi : formula) : subformula (Forall v psi) phi -> subformula psi phi.
