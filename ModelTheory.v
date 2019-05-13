c17784df62ea6dfc4ea307bd4919c8db4054bd99
afichman <andrew.fichman@gmail.com>
2019-05-12 20:11:41 -0400

    changed from Ensemble to list for simplicity; added subst example; changed relations to be numbered

--- ModelTheory.v
+++ ModelTheory.v
@@ -1,6 +1,5 @@
 Require Export Coq.Strings.String.
 Require Export List.
-Require Export Coq.Sets.Ensembles.
 Require Export Coq.Bool.Bool.
 Require Export Coq.Arith.EqNat.
 
@@ -27,7 +26,7 @@ Inductive term : Type :=
 (* FixMe: does this need a type param? *)
 Inductive formula : Type := 
 | Equals  : term -> term -> formula
-| Relates : string -> (list term) -> formula
+| Relates : nat -> (list term) -> formula
 | Not     : formula -> formula
 | Or      : formula -> formula -> formula
 | Forall  : var -> formula -> formula.
@@ -118,55 +117,7 @@ Proof.
   assumption.
 Qed.
 
-Fixpoint term_vars (t : term) := match t with
-| VarT v    => Singleton var v
-| ConstT c  => Empty_set var
-| FuncT f l => fold_left (fun a x => Union var a (term_vars x)) l (Empty_set var) 
-end.
-
-Module term_vars_example.
-Definition t  := FuncT (Func 1) (VarT (Var 1)::(VarT (Var 1))::nil).
-Definition tv := term_vars t.
-Theorem TermVarsExample : In var tv (Var 1).
-Proof.
-  unfold In.
-  unfold tv.
-  simpl.
-  apply Union_intror.
-  unfold In.
-  apply In_singleton.
-Qed.
-End term_vars_example.
-
-Fixpoint free_vars (phi : formula) := match phi with 
-| Equals t1 t2  => Union var (term_vars t1) (term_vars t2)
-| Relates _ l   => fold_left (fun a x => Union var a (term_vars x)) l (Empty_set var)
-| Not psi       => free_vars psi
-| Or theta psi  => Union var (free_vars theta) (free_vars psi)
-| Forall v psi  => Subtract var (free_vars psi) v
-end.
-
-Module free_vars_example.
-Definition v    := VarT (Var 1).
-Definition t    := FuncT (Func 1) (v::nil). 
-Definition phi  := Forall (Var 1) (Equals t t).
-Definition fv   := free_vars phi.
-Theorem FreeVarsExample :  ~ (In var fv (Var 1)).
-Proof.
-  unfold not.
-  intros.
-  unfold In in *.
-  unfold fv in *.
-  simpl in *.
-  unfold Subtract in *.
-  unfold Setminus in *.
-  unfold In in *.
-  destruct H.
-  destruct H0.
-  apply In_singleton.
-Qed.
-
-
+(* Util functions for var lists *)
 Definition eq_var (v1 : var) (v2 : var) := match v1, v2 with
 | Var v1', Var v2' => beq_nat v1' v2'
 end.
@@ -181,16 +132,62 @@ Fixpoint remove_var (l : list var) (v : var) := match l with
 | h::t => if eq_var v h then remove_var t v else h::(remove_var t v)
 end.
 
+(* Returns vars in a term *)
+Fixpoint term_vars (t : term) := match t with
+| VarT v    => v::[]
+| ConstT c  => []
+| FuncT f l => fold_left (fun a x => a++(term_vars x)) l []
+end.
+
+Module term_vars_example.
+Definition t  := FuncT (Func 1) (VarT (Var 1)::(VarT (Var 1))::nil).
+Definition tv := term_vars t.
+Theorem thm : contains_var tv (Var 1) = true. Proof. reflexivity. Qed.
+End term_vars_example.
+
+(* Returns free vars *)
+Fixpoint free_vars (phi : formula) := match phi with 
+| Equals t1 t2  => (term_vars t1)++(term_vars t2)
+| Relates _ l   => fold_left (fun a x => a++(term_vars x)) l []
+| Not psi       => free_vars psi
+| Or theta psi  => (free_vars theta)++(free_vars psi)
+| Forall v psi  => remove_var (free_vars psi) v
+end.
+
+Module free_vars_example.
+Definition v1   := VarT (Var 1).
+Definition v2   := VarT (Var 2).
+Definition t    := FuncT (Func 1) (v1::nil). 
+Definition phi  := Or (Equals v2 v2) (Forall (Var 1) (Equals t t)).
+Definition fv   := free_vars phi.
+Theorem thm1 : contains_var fv (Var 1) = false. Proof. reflexivity. Qed.
+Theorem thm2 : contains_var fv (Var 2) = true. Proof. reflexivity. Qed.
+End free_vars_example.
+
+(* Performs a substitution of a variable with a term *)
 Fixpoint subst_term (t_orig : term) (v : var) (t : term) := match t_orig with
 | VarT v    => t
 | ConstT c  => ConstT c
 | FuncT f l => FuncT f (fold_left (fun a x => (subst_term x v t)::a) l nil)
 end.
 
-Fixpoint subst (phi: formula) (v : var) (t : term) (free : list var) := match phi with
+Fixpoint subst_helper (phi: formula) (v : var) (t : term) (free : list var) := match phi with
 | Equals t1 t2 => if (contains_var free v) then Equals (subst_term t1 v t) (subst_term t2 v t) else Equals t1 t2
 | Relates r l => if (contains_var free v) then Relates r (fold_left (fun a x => (subst_term x v t)::a) l nil) else Relates r l
-| Not psi => Not (subst psi v t free)
-| Or theta psi => Or (subst theta v t free) (subst psi v t free)
-| Forall v psi => Forall v (subst psi v t (remove_var free v))
+| Not psi => Not (subst_helper psi v t free)
+| Or theta psi => Or (subst_helper theta v t free) (subst_helper psi v t free)
+| Forall v psi => Forall v (subst_helper psi v t (remove_var free v))
 end.
+
+Definition subst (phi: formula) (v : var) (t : term) := subst_helper phi v t (free_vars phi).
+
+Module subst_example.
+Definition v1       := Var 1.
+Definition v1T      := VarT v1.
+Definition v2T      := VarT (Var 2).
+Definition cT       := ConstT (Const 1).
+Definition phi      := Or (Equals v1T v1T) (Forall v1 (Relates 1 [v1T; v2T])).
+Definition phi_sub  := subst phi v1 cT.
+Definition expected := Or (Equals cT cT) (Forall v1 (Relates 1 [v1T; v2T])).
+Theorem thm1 : phi_sub = expected. Proof. reflexivity. Qed.
+End subst_example.
